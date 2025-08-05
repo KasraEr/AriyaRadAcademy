ba9edcce5ce8e482812ad7f40af557be2ed0fe2c
KasraEr <erfanian.kasra@hitmail.com>
2025-08-05 17:42:14 +0330

    making ui

--- src/components/templates/Profile.jsx
+++ src/components/templates/Profile.jsx
@@ -1,5 +1,12 @@
+import { getToken } from "../../utils/tokenService"
+import { jwtDecode } from "jwt-decode"
 
 export default function Profile() {
+
+  const token = getToken()
+  const details = jwtDecode(token)
+  console.log(details)
+
   return (
     <div>Profile</div>
   )
